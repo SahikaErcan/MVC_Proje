9b3be3058ff27ed6b8f7bca98c5219d090d650b7
SahikaErcan <72580629+SahikaErcan@users.noreply.github.com>
2023-06-27 02:04:06 +0300

    MVC

---
+++ DataAccessLayer/Abstract/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{
+    public interface IRepository<T>
+    {
+        // CRUD
+        // Listeleme, Ekleme, Güncelleme, Silme
+        List<T> List();
+        void Insert(T p);
+        void Delete(T p);
+        void Update(T p);
+
+        // Şartlı listeleme - Yazar adı Ali olanları getir gibi..
+        List<T> List(Expression<Func<T, bool>> expression);
+    }
+}
